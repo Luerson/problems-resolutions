f424866b6c7d6ae1f9ffd2c9996cf3d62de009f0
Luerson de Albuquerque Silva Filho <92219445+Luerson@users.noreply.github.com>
2021-10-23 16:52:54 -0300

    1176

---
+++ 1176.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * @brief Esta função calcula o valor de fibonacci em uma determinada posição
+ * @param posicao corresponde à posição na sequência de fibonacci do valor que
+ * se quer calcular. '0' = primeira posição, '1' = segunda posição...
+ * @return valores naturais da sequência de fibonacci
+ */
+unsigned long long ValorDaPosicao(posicao){
+    unsigned long long novo = 1, anterior = 0;
+
+    if(posicao == 0){
+        return 0;
+    }
+
+    posicao -= 1;
+
+    while(posicao--){           
+        novo += anterior;
+        anterior = novo - anterior;
+    }
+    
+
+    return novo;
+}
+
+int main(){
+    int valorT, posicao;//entrada
+
+    scanf("%d", &valorT);
+
+    while(valorT--){
+        scanf("%d", &posicao);
+        printf("Fib(%d) = %llu\n", posicao, ValorDaPosicao(posicao));
+    }
+
+    return 0;
+}
