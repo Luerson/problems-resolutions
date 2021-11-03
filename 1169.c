2b55bd35cb096f808fa1aebf7340180c2ad635e8
Luerson de Albuquerque Silva Filho <92219445+Luerson@users.noreply.github.com>
2021-11-03 19:22:24 -0300

    Create 1169.c

---
+++ 1169.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * @brief Esta função calcula o peso dos grãos que serão
+ * colocados no tabuleiro 
+ * 
+ * @param nCasas numero de casas do tabuleiro de damas
+ * 
+ * @return retorna o peso total dos grãos em quilos(Kg) 
+ */
+unsigned long long GraosNoTabuleiro(unsigned long long nCasas){
+    unsigned long long graos;
+
+    graos = pow(2, nCasas) - 1;
+
+    return graos/12000;
+}
+
+int main(){
+    int N, valorX;
+
+    scanf("%d", &N);
+
+    while(N--){
+        scanf("%d", &valorX);
+
+        printf("%llu kg\n", GraosNoTabuleiro(valorX));
+    }
+
+    return 0;
+}
